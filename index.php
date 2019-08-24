f60234f48b4ed8f205e78dda986afbd93fc1497e
Mert Ocak <mertocak@Merts-MacBook-Pro.local>
2019-08-24 05:52:53 +0300

    ADD

--- index.php
+++ index.php
@@ -1,3 +1,32 @@
+<?php
+session_start();
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+include_once ($_SERVER['DOCUMENT_ROOT'] . '/admin/config/config.php');
+
+
+$charset = 'utf8';
+
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$options = [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES => false,
+];
+try {
+    $pdo = new PDO($dsn, $user, $pass, $options);
+} catch (\PDOException $e) {
+    throw new \PDOException($e->getMessage(), (int)$e->getCode());
+}
+
+$banner = $pdo->query("SELECT * FROM banners WHERE id=1")->fetch();
+$bannerImage = $pdo->query("SELECT Path FROM photos WHERE Family = 'banners'")->fetch();
+$settings = $pdo->query("SELECT * FROM settings WHERE id = 1")->fetch();
+
+?>
+
 <!doctype html>
 <html lang="en">
 <head>
@@ -11,7 +40,29 @@
     <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap-grid.css">
     <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap-reboot.css">
     <link rel="stylesheet" href="assets/css/tekurunplus.css">
+    <style>
+        .theme-color-bg {
+            background: #<?php echo $settings['ThemeColor']; ?> !important;
+            border-color: #<?php echo $settings['ThemeColor']; ?> !important;
+        }
 
+        .theme-color-only-border {
+            border-color: #<?php echo $settings['ThemeColor']; ?> !important;
+        }
+
+        .theme-color-text {
+            color: #<?php echo $settings['ThemeColor']; ?> !important;
+            border-color: #<?php echo $settings['ThemeColor']; ?> !important;
+        }
+
+        .text-color {
+            color: #<?php echo $settings['TextColor']; ?> !important;
+        }
+
+        body .container input:checked ~ .checkmark {
+            background-color: #<?php echo $settings['ThemeColor']; ?> !important;
+        }
+    </style>
 </head>
 <body>
 <div class="main-wrapper">
@@ -20,43 +71,43 @@
         <div class="row">
             <div class="col-7 col-md-4 logo-wrapper">
                 <div id="logo">
-                    <div class="brand-name">nutiva</div>
-                    <div class="description">Kokonat Yağı</div>
+                    <div class="brand-name theme-color-text"><?php echo $banner['LogoText1']; ?></div>
+                    <div class="description text-color"><?php echo $banner['LogoText2']; ?></div>
                 </div>
             </div>
             <div class="col-5 col-md-8">
                 <div id="menu">
-                    <a href="#">Anasayfa</a> <span> / </span>
-                    <a href="#">Nedir?</a> <span> / </span>
-                    <a href="#">Ne işe yarar?</a> <span> / </span>
-                    <a href="#">Fiyatlar</a>
+                    <a class="theme-color-text" href="#">Anasayfa</a> <span class="theme-color-text"> / </span>
+                    <a class="theme-color-text" href="#">Nedir?</a> <span class="theme-color-text"> / </span>
+                    <a class="theme-color-text" href="#">Ne işe yarar?</a> <span class="theme-color-text"> / </span>
+                    <a class="theme-color-text" href="#">Fiyatlar</a>
                 </div>
                 <div id="hamburger">
                     <div></div>
                     <div></div>
                     <div></div>
                 </div>
-                <div id="buy-now" class="btn-tekurunplus float-right">Satın Al</div>
+                <div id="buy-now" class="btn-tekurunplus float-right theme-color-bg">Satın Al</div>
             </div>
         </div>
     </nav>
 
     <section id="banner">
         <div id="banner-top" class="row">
-            <div class="alt-banner d-block d-lg-none">%100 Doğal, Katkısız</div>
+            <div class="alt-banner d-block d-lg-none text-color"><?php echo $banner['SloganText3']; ?></div>
 
             <div class="bannerImageHolder col-12 d-md-block col-lg-6 text-right"><img class="img-fluid"
-                                                                                      src="assets/img/tekurunAsset 8.png"/>
+                                                                                      src="<?php echo $bannerImage['Path']; ?>"/>
             </div>
             <div class="col-12 col-lg-6 second-area">
-                <div class="brand-banner  d-none d-lg-block">nutiva</div>
-                <div class="desc-banner  d-none d-lg-block">Kokonat Yağı</div>
-                <div class="alt-banner d-none d-lg-block">%100 Doğal, Katkısız</div>
+                <div class="brand-banner  d-none d-lg-block theme-color-text"><?php echo $banner['SloganText1']; ?></div>
+                <div class="desc-banner  d-none d-lg-block text-color"><?php echo $banner['SloganText2']; ?></div>
+                <div class="alt-banner d-none d-lg-block text-color"><?php echo $banner['SloganText3']; ?></div>
                 <div class="buttons">
-                    <div class="btn-tekurunplus-outline">
+                    <div class="btn-tekurunplus-outline theme-color-text">
                         İncele
                     </div>
-                    <div class="btn-tekurunplus btn-big font-weight-bold">
+                    <div class="btn-tekurunplus btn-big font-weight-bold theme-color-bg">
                         SATIN AL
                     </div>
                 </div>
@@ -65,7 +116,7 @@
         </div>
     </section>
 </div>
-<section class="divider first text-center">
+<section class="divider first text-center theme-color-bg">
 
     <h1>Yeni içeriğiyle raflarda !</h1>
 
@@ -74,13 +125,13 @@
 </section>
 <div class="main-wrapper">
     <div id="content-1">
-        <h1>Nedir?</h1>
-        <h2>Kokonat yağı nedir?</h2>
+        <h1 class="theme-color-text">Nedir?</h1>
+        <h2 class="text-color">Kokonat yağı nedir?</h2>
         <div class="col-12 m-auto product-image">
             <img class="img-fluid" src="assets/img/tekurunAsset 3.png"/>
         </div>
 
-        <div class="text">
+        <div class="text text-color">
             <span>Hindistan cevizi yağı,</span> antik çağlardan beri Asya'da ekstre halinde <span
                 class="d-none d-md-inline-block"><br></span>
             imal edilmiştir. Hindistan cevizi ve zeytin,
@@ -100,7 +151,7 @@
         </div>
     </div>
 </div>
-<section class="divider regular text-center">
+<section class="divider regular text-center theme-color-bg">
 
     <h1>Yeni içeriğiyle raflarda !</h1>
 
@@ -109,13 +160,13 @@
 </section>
 <div class="main-wrapper">
     <div id="content-2">
-        <h1>Ne işe yarar?</h1>
-        <h2>Kokonat yağı ne işe yarar?</h2>
+        <h1 class="theme-color-text">Ne işe yarar?</h1>
+        <h2 class="text-color">Kokonat yağı ne işe yarar?</h2>
 
         <div class="col-12 m-auto product-image">
             <img class="img-fluid" src="assets/img/tekurunAsset 3.png"/>
         </div>
-        <div class="text">
+        <div class="text text-color">
             <span>Hindistan cevizi yağı,</span> antik çağlardan beri Asya'da ekstre halinde <span
                 class="d-none d-md-inline-block"><br></span>
             imal edilmiştir. Hindistan cevizi ve zeytin,
@@ -139,7 +190,7 @@
         </div>-->
     </div>
 </div>
-<section class="divider regular text-center">
+<section class="divider regular text-center theme-color-bg">
 
     <h1>Yeni içeriğiyle raflarda !</h1>
 
@@ -148,8 +199,8 @@
 </section>
 <div class="main-wrapper">
     <div id="content-3">
-        <h1>Fiyatlar</h1>
-        <h2>Yeni içerik, yeni fiyat!</h2>
+        <h1 class="theme-color-text">Fiyatlar</h1>
+        <h2 class="text-color">Yeni içerik, yeni fiyat!</h2>
  <!--       <div class="col-12 ml-auto mr-auto mt-4 product-image">
             <img class="img-fluid m-auto" src="assets/img/tekurunAsset 9.png"/>
         </div>-->
@@ -159,14 +210,14 @@
                     <div class="col-12">
                         <div>
                             <img class="down" src="assets/img/urunAsset 12.png" width="100"/></div>
-                     <div class="amount">1 ADET</div>
+                     <div class="amount theme-color-text">1 ADET</div>
                         <div class="col-12 pricePhoto text-center">
 
                             <img class="img-fluid" src="assets/img/urunAsset 8.png"/></div>
-                        <div class="priceTag">59₺</div>
+                        <div class="priceTag theme-color-text">59₺</div>
                     </div>
                     <div class="col-12">
-                        <div id="buy-now" class="btn-tekurunplus m-auto priceBuy">Satın Al</div>
+                        <div id="buy-now" class="btn-tekurunplus m-auto priceBuy theme-color-bg">Satın Al</div>
                     </div>
                 </div>
             </div>
@@ -175,14 +226,14 @@
                     <div class="col-12">
                         <div>
                             <img class="down" src="assets/img/urunAsset 12.png" width="100"/></div>
-                        <div class="amount">2 ADET</div>
+                        <div class="amount theme-color-text">2 ADET</div>
                         <div class="col-12 pricePhoto two text-center">
 
                             <img class="img-fluid" src="assets/img/urunAsset 10.png"/></div>
-                        <div class="priceTag">109₺</div>
+                        <div class="priceTag theme-color-text">109₺</div>
                     </div>
                     <div class="col-12">
-                        <div id="buy-now" class="btn-tekurunplus m-auto priceBuy">Satın Al</div>
+                        <div id="buy-now" class="btn-tekurunplus m-auto priceBuy theme-color-bg">Satın Al</div>
                     </div>
                 </div>
             </div>
@@ -191,14 +242,14 @@
                     <div class="col-12">
                         <div>
                             <img class="down" src="assets/img/urunAsset 12.png" width="100"/></div>
-                        <div class="amount">3 ADET</div>
+                        <div class="amount theme-color-text">3 ADET</div>
                         <div class="col-12 pricePhoto three text-center">
 
                             <img class="img-fluid" src="assets/img/urunAsset 11.png"/></div>
-                        <div class="priceTag">159₺</div>
+                        <div class="priceTag theme-color-text">159₺</div>
                     </div>
                     <div class="col-12">
-                        <div id="buy-now" class="btn-tekurunplus m-auto priceBuy">Satın Al</div>
+                        <div id="buy-now" class="btn-tekurunplus m-auto priceBuy theme-color-bg">Satın Al</div>
                     </div>
                 </div>
             </div>
@@ -206,7 +257,7 @@
         </div>
     </div>
 </div>
-<section class="divider regular text-center">
+<section class="divider regular text-center theme-color-bg">
 
     <h1>Yeni içeriğiyle raflarda !</h1>
 
@@ -220,7 +271,7 @@
         </div>
     </div>
 </div>
-<section class="divider order_ text-center">
+<section class="divider order_ text-center theme-color-bg">
     <div class="order">
         <div class="row">
             <div class="col-12">
@@ -252,59 +303,59 @@
                     </div>
                     <div class="form-row mt-5">
                         <div class="col-12 d-md-none">
-                            Kişisel Bilgiler
+                            <span class="text-color">Kişisel Bilgiler</span>
                             <hr>
                         </div>
                         <div class="col-12 col-md-6">
-                            <label for="FirstName">Ad</label>
-                            <input type="text" class="form-control" id="FirstName" name="FirstName" autocomplete="off">
+                            <label class="theme-color-text" for="FirstName">Ad</label>
+                            <input type="text" class="form-control theme-color-only-border" id="FirstName" name="FirstName" autocomplete="off">
                         </div>
                         <div class="col-12 col-md-6">
-                            <label for="Soyad">Soyad</label>
-                            <input type="text" class="form-control" id="Soyad" name="Soyad" autocomplete="off">
+                            <label class="theme-color-text" for="Soyad">Soyad</label>
+                            <input type="text" class="form-control theme-color-only-border" id="Soyad" name="Soyad" autocomplete="off">
                         </div>
                     </div>
                     <div class="form-row">
                         <div class="col-12 d-md-none mt-5">
-                            İletişim Bilgileri
+                            <span class="text-colort">İletişim Bilgileri</span>
                             <hr>
                         </div>
                         <div class="col-12 col-md-6">
-                            <label for="Mail">E-Posta adresi</label>
-                            <input type="text" class="form-control" id="Mail" name="Mail" autocomplete="off">
+                            <label class="theme-color-text" for="Mail">E-Posta adresi</label>
+                            <input type="text" class="form-control theme-color-only-border" id="Mail" name="Mail" autocomplete="off">
                         </div>
                         <div class="col-12 col-md-6">
-                            <label for="telefon-numaranız">Telefon Numarası</label>
-                            <input type="text" class="form-control" id="telefon-numaranız" name="Phone"
+                            <label class="theme-color-text" for="telefon-numaranız">Telefon Numarası</label>
+                            <input type="text" class="form-control theme-color-only-border" id="telefon-numaranız" name="Phone"
                                    autocomplete="off">
                         </div>
                     </div>
                     <div class="form-row">
                         <div class="col-12 d-md-none mt-5">
-                            Sipariş Detayları
+                            <span class="text-color">Sipariş Detayları</span>
                             <hr>
                         </div>
                         <div class="col-12 col-md-6">
-                            <label for="Name">Ürün Seçiniz</label>
-                            <select class="form-control" id="Name" name="Name">
+                            <label class="theme-color-text" for="Name">Ürün Seçiniz</label>
+                            <select class="form-control theme-color-only-border" id="Name" name="Name">
                                 <option value="1">1 adet Nutiva kokonat yağı</option>
                             </select>
                         </div>
 
                         <div class="col-12 col-md-6">
-                            <label for="PaymentId">Ödeme Şekli Seçiniz</label>
-                            <select class="form-control" id="PaymentId" name="PaymentId">
+                            <label class="theme-color-text" for="PaymentId">Ödeme Şekli Seçiniz</label>
+                            <select class="form-control theme-color-only-border" id="PaymentId" name="PaymentId">
                                 <option value="1">Online Ödeme</option>
                             </select>
                         </div>
                     </div>
                     <div class="form-group">
                         <div class="col-12 d-md-none mt-5">
-                            Teslimat Bilgileri
+                            <span class="text-color">Teslimat Bilgileri</span>
                             <hr>
                         </div>
-                        <label class="tal" for="teslimat-adresi">Teslimat Adresi</label>
-                        <textarea class="form-control" id="teslimat-adresi" name="Address"
+                        <label class="tal theme-color-text" for="teslimat-adresi">Teslimat Adresi</label>
+                        <textarea class="form-control theme-color-only-border" id="teslimat-adresi" name="Address"
                                   autocomplete="nope"></textarea>
                     </div>
                     <div class="form-group" style="margin-bottom: 0px !important;">
@@ -314,7 +365,7 @@
                                   <label class="form-check-label" for="Agreement">
                                   </label> -->
 
-                                <label class="container form-check-label mb-3"><span>Mesafeli satış sözleşmesi</span>'ni
+                                <label class="container form-check-label mb-3 theme-color-text"><span>Mesafeli satış sözleşmesi</span>'ni
                                     okudum, kabul ediyorum.
 
                                     <input type="checkbox">
@@ -322,7 +373,7 @@
                                 </label>
                             </div>
                             <div class="col-12 col-md-4 text-right">
-                                <input class="btn-form" type="submit" value="SİPARİŞİ TAMAMLA">
+                                <input class="btn-form theme-color-bg" type="submit" value="SİPARİŞİ TAMAMLA">
                             </div>
                         </div>
                     </div>
@@ -336,13 +387,13 @@
                     <div style="clear:both;"></div>
                 </form>
             </div>
-            <div class="col-hidden col-lg-5 formImage">
+            <div class="col-hidden col-lg-5 formImage" style="background-image: url(<?php echo $bannerImage['Path']; ?>)">
             </div>
         </div>
 
     </div>
 </div>
-<section class="divider regular text-center mt-0">
+<section class="divider regular text-center mt-0 theme-color-bg">
 
     <div id="footer">
         <div class="row footer-links text-left">
